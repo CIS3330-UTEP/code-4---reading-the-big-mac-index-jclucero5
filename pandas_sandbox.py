ae5cacbbfa256f9c05143b699a7b0ceb62f99abf
jclucero5 <Jclucero5@miners.utep.edu>
2024-02-14 12:28:14 -0700

    Code_4.py

---
+++ pandas_sandbox.py
@@ -0,0 +1,14 @@
+import pandas 
+
+list_teams = ['49ers','KC','Cowboys', 'Steelers']
+
+print(type(list_teams))
+
+print(list_teams)
+
+series_team = pandas.Series(list_teams)
+
+print(series_team)
+print(type(series_team))
+
+
